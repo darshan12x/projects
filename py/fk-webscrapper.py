f3c2d385b1512f5bd7bb408c6ec1eaf41f83c431
A <darsa@amazon.com>
2020-04-28 20:31:21 +0530

    Essential Mundane Tasks

--- py/fk-webscrapper.py
+++ py/fk-webscrapper.py
@@ -7,8 +7,8 @@ prices = []  # List to store price of the product
 ratings = []  # List to store rating of the product
 driver.get("https://www.flipkart.com/home-kitchen/home-appliances/washing-machines/fully-automatic-front-load~function/pr?sid=j9e%2Cabm%2C8qx&otracker=nmenu_sub_TVs%20%26%20Appliances_0_Fully%20Automatic%20Front%20Load")
 content = driver.page_source
-soup = BeautifulSoup(content)
-print(soup)
+soup = BeautifulSoup(content, "html.parser")
+# print(soup.encode("utf-8"))
 for a in soup.findAll('a', href=True, attrs={'class': '_31qSD5'}):
     name = a.find('div', attrs={'class': '_3wU53n'})
     price = a.find('div', attrs={'class': '_1vC4OE _2rQ-NK'})
@@ -18,3 +18,4 @@ for a in soup.findAll('a', href=True, attrs={'class': '_31qSD5'}):
     ratings.append(rating.text)
 df = pd.DataFrame({'Product Name': products, 'Price': prices, 'Rating': ratings})
 df.to_csv('products.csv', index=False, encoding='utf-8')
+driver.quit()
